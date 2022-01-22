98fa222568549f1fb3dfd751da33e2c75d53efd4
Gehrig Kunz <gakz@gehrigs-mbp.lan>
2022-01-22 10:55:23 -0700

    First upload

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Link from 'next/link'
+import Link from "next/link";
+
 const Index = () => (
-  <div>
-    Hello World.{' '}
+  <div className="container">
+    {/* 
     <Link href="/about">
       <a>About</a>
     </Link>
+    */}
+    <h1>Gehrig</h1>
   </div>
-)
+);
 export default Index;
